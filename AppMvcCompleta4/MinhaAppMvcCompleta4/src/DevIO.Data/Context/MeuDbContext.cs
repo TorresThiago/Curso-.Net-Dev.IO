339a6cfaea6398855156b8aa223ddd64ab5fa5fa
Thiago Torres <thiago.torres.ter@estacio.br>
2021-08-03 16:39:16 -0300

    03/08/2021 - Curso .Net Core / Módulo App Completa / Aula: 08 - Scaffolding das Controllers e Views

---
+++ AppMvcCompleta4/MinhaAppMvcCompleta4/src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Uma garantia para que crie-se um novo campo como varchar100 e não NVARCHAR pois onera o BD
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            }
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                //Esta sendo impedido ao excluir o iten, que leve os filhos junto. (Desabilitar o cascade delete) ClientSetNull
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
