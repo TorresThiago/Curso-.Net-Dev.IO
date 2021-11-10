8fdfaa43ab4ce49e26545a262e603616009482fa
Thiago Torres <thiago.torres.ter@estacio.br>
2021-11-10 13:20:38 -0300

    Aula Finalizando o ajuste das Views

---
+++ AppMvcCompleta4/MinhaAppMvcCompleta4/src/DevIO.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DevIO.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormatDocumento(this RazorPage page, int tipoPessoa, string documento)
+        {
+            return tipoPessoa == 1
+                ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00")
+                : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+    }
+}
